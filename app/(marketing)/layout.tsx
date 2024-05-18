ed172a178d86c0e7f62ea6bf5edb2512eed91dd1
ToiznerD <toiznerd@gmail.com>
2024-05-18 15:01:39 +0300

    first commit

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,23 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Footer } from "./footer";
+import { Header } from "./header";
+
+type Props = {
+    children: React.ReactNode;
+}
+
+const MarketingLayout = ({ children } : Props) => {
+    return (
+        <ClerkProvider>
+            <div className="min-h-screen flex flex-col">
+                <Header />
+                <main className="flex-1 flex flex-col items-center justify-center">
+                    {children}
+                </main>
+                <Footer />
+                </div>
+        </ClerkProvider>
+    )
+}
+
+export default MarketingLayout;
