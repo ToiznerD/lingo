a3d5262a11bbc2c67b4c85295fd06e641ce05f41
ToiznerD <toiznerd@gmail.com>
2024-05-29 00:37:51 +0300

    lesson header and exit modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { ExitModal } from "@/components/modals/exit-modal";
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <Toaster />
+        <ExitModal />
         {children}
       </body>
     </html>
